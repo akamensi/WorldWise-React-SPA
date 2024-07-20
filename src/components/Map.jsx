75e91cbaf81c5f538a9c8bc6a02b73b879e68f8b
akamensi <akez.muhammed@gmail.com>
2024-07-20 18:03:32 +0200

    Reading and Setting a Query String from URL

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        position: {lat}, {lng}
+      </h2>
+    </div>
+  );
 }
 
 export default Map;
