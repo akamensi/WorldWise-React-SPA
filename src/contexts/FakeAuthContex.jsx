d5071a2115dabecc739f29e3ba80cd2cc4104d91
akamensi <akez.muhammed@gmail.com>
2024-07-28 18:00:04 +0200

    Adding fake Authentication: Implemeting Login

--- src/contexts/FakeAuthContex.jsx
+++ src/contexts/FakeAuthContex.jsx
@@ -49,6 +49,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContex";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const { login, isAuth } = useAuth();
+  const navigate = useNavigate();
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  function handleSumbit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuth) navigate("/app", { replace: true });
+    },
+    [isAuth, navigate]
+  );
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSumbit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +49,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
