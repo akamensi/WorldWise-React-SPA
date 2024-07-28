0885bcf45b014f6ed254b4d38e399c1e018434f2
akamensi <akez.muhammed@gmail.com>
2024-07-28 18:25:53 +0200

    Adding fake Authentication: Protecting a Route

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContex";
+import { useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (!isAuth) navigate("/");
+    },
+    [isAuth, navigate]
+  );
+  return isAuth ? children : null;
+}
+
+export default ProtectedRoute;
